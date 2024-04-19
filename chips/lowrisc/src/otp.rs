c59345f3c390a93a6de838d8c521b24bf2ea8d99
Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
2024-04-19 14:31:08 +0300

    Implemented read operation

--- chips/lowrisc/src/otp.rs
+++ chips/lowrisc/src/otp.rs
@@ -8,9 +8,10 @@
 use crate::create_non_zero_usize;
 
 use crate::registers::otp_ctrl_regs::{
-    OtpCtrlRegisters, CHECK_REGWEN
+    OtpCtrlRegisters, CHECK_REGWEN, DIRECT_ACCESS_ADDRESS, DIRECT_ACCESS_CMD, STATUS,
 };
 
+use kernel::ErrorCode;
 use kernel::utilities::StaticRef;
 use kernel::utilities::registers::interfaces::{Readable, ReadWriteable, Writeable};
 
@@ -18,6 +19,41 @@ use core::num::NonZeroUsize;
 
 const NUMBER_ERRORS: NonZeroUsize = create_non_zero_usize!(14);
 const MASK_ERRORS: NonZeroUsize = create_non_zero_usize!((1 << NUMBER_ERRORS.get()) - 1);
+const MAX_PAST_OTP_ADDRESS: NonZeroUsize = create_non_zero_usize!(2048);
+
+/// Address of a 32-bit word
+pub struct OtpAddress32(u32);
+
+impl OtpAddress32 {
+    /// Create a new OTP address for a 32-bit word.
+    ///
+    /// # Parameters
+    ///
+    /// + `raw_address`: the value that should represent an OTP address
+    ///
+    /// # Return value
+    ///
+    /// + Ok(Self): the OTP address
+    /// + Err(()): if `raw_address` does not fit in the 10-bit address space and is not properly
+    /// aligned.
+    pub const fn new(raw_address: u32) -> Result<Self, ()> {
+        // CAST: u32 == usize on RV32I
+        if raw_address >= MAX_PAST_OTP_ADDRESS.get() as u32 || raw_address & 0b11 != 0 {
+            Err(())
+        } else {
+            Ok(Self(raw_address))
+        }
+    }
+
+    /// Convert the [OtpAddress32] to a 32-bit unsigned number
+    ///
+    /// # Return value
+    ///
+    /// The underlying 32-bit unsigned number.
+    pub const fn as_u32(self) -> u32 {
+        self.0
+    }
+}
 
 pub struct Otp {
     registers: StaticRef<OtpCtrlRegisters>
@@ -39,19 +75,19 @@ impl Otp {
         }
     }
 
-    /// Check whether an error occurred during peripheral initialization.
+    /// Check whether an error occurred.
     ///
     /// # Return value
     ///
     /// + Ok(()): no errors
     /// + Err(()): the peripheral encountered an error
-    fn check_init_errors(&self) -> Result<(), ()> {
+    fn has_errors(&self) -> bool {
         let status_value = self.registers.status.get();
         // CAST: usize == u32 on RV32I
         if status_value & (MASK_ERRORS.get() as u32) != 0 {
-            Err(())
+            true
         } else {
-            Ok(())
+            false
         }
     }
 
@@ -106,7 +142,9 @@ impl Otp {
         consistency_check_period: u32,
         timeout: u32
     ) -> Result<(), ()> {
-        self.check_init_errors()?;
+        if self.has_errors() {
+            return Err(());
+        }
         self.set_integrity_check_period(integrity_check_period);
         self.set_consistency_check_period(consistency_check_period);
         // Check timeout doesn't seem to work.
@@ -116,5 +154,63 @@ impl Otp {
         self.lock_check_registers();
 
         Ok(())
+    }
+
+    /// Check whether the peripheral is busy
+    ///
+    /// # Return value
+    ///
+    /// + false: the peripheral is idle
+    /// + true: the peripheral is busy
+    fn is_busy(&self) -> bool {
+        !self.registers.status.is_set(STATUS::DAI_IDLE)
+    }
+
+    /// Set the address for the next operation
+    fn set_address32(&self, address: OtpAddress32) {
+        self.registers.direct_access_address.modify(DIRECT_ACCESS_ADDRESS::DIRECT_ACCESS_ADDRESS.val(address.as_u32()));
+    }
+
+    /// Start a read
+    fn start_read(&self) {
+        self.registers.direct_access_cmd.modify(DIRECT_ACCESS_CMD::RD::SET);
+    }
+
+    /// Get a 32-bit word from DIRECT_ACCESS_RDATA register
+    ///
+    /// # Return value
+    ///
+    /// The bottom half of DIRECT_ACCESS_RDATA
+    fn get_word32(&self) -> u32 {
+        self.registers.direct_access_rdata[0].get()
+    }
+
+    /// Read a 32-bit word.
+    ///
+    /// # Parameters
+    ///
+    /// + `address`: the address of the word to be read
+    ///
+    /// # Return value
+    ///
+    /// + Ok(u32): the read value
+    /// + Err(ErrorCode): an error occurred:
+    ///     + ErrorCode::BUSY: the peripheral is busy
+    ///     + ErrorCode::FAIL: the read operation failed
+    pub fn read_word32(&self, address: OtpAddress32) -> Result<u32, ErrorCode> {
+        if self.is_busy() {
+            return Err(ErrorCode::BUSY);
+        }
+
+        self.set_address32(address);
+        self.start_read();
+
+        while self.is_busy() {}
+
+        if self.has_errors() {
+            return Err(ErrorCode::FAIL);
+        }
+
+        Ok(self.get_word32())
     }
 }
