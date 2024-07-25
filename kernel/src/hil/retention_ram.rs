3e339a161180b188bafd95fc78e69e07346a98bf
Vlad Radulescu <vlad.here@gmail.com>
2024-07-25 15:39:02 +0300

    Small cleanup and licensing.

--- kernel/src/hil/retention_ram.rs
+++ kernel/src/hil/retention_ram.rs
@@ -1,10 +1,9 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2022.
+
 use crate::ErrorCode;
 
-// pub enum RetentionError {
-//     OutOfRange,
-//     NotInintialized,
-//     Failed,
-// }
 pub trait OwnerRetentionRam {
     type Data: Copy;
     type ID: Copy;
