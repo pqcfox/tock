ec70251952078eefd49df3aac57662ff299d3bce
Vlad Radulescu <vlad.here@gmail.com>
2024-10-11 14:22:44 +0300

    Increased stack, now tes tests run.

--- boards/opentitan/src/main.rs
+++ boards/opentitan/src/main.rs
@@ -206,7 +206,7 @@ const FAULT_RESPONSE: capsules_system::process_policies::PanicFaultPolicy =
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
 #[link_section = ".stack_buffer"]
-pub static mut STACK_MEMORY: [u8; 0x1400] = [0; 0x1400];
+pub static mut STACK_MEMORY: [u8; 0x2000] = [0; 0x2000];
 
 /// A structure representing this platform that holds references to all
 /// capsules for this platform. We've included an alarm and console.
